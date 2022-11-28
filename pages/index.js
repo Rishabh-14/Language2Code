883e4ac6ac33fa97c5628034aae6677b9ca1adff
Shelabh <68127067+shelabh@users.noreply.github.com>
2022-11-29 01:35:14 +0530

    added backend

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react'
 
 const Home = () => {
+  const[input, setInput] =  useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const toggleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ input }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
   return (
     <div className="root">
       <Head>
@@ -11,15 +41,42 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>English to Code</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>"English to Code" is a guide to translating English phrases into code.</h2>
           </div>
+        </div>
+        <div className="prompt-container">
+          <textarea 
+            placeholder="Code of..." 
+            className="prompt-box" 
+            value={input}
+            onChange={toggleChange}
+          />
+          <div className="prompt-buttons">
+            <a className={isGenerating ? "generate-button loading" : "generate-button"} onClick={callGenerateEndpoint}>
+              <div className="generate">
+                {isGenerating ? <span class="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="badge-container grow">
-        <a
+        {/* <a
           href="https://buildspace.so/builds/ai-writer"
           target="_blank"
           rel="noreferrer"
@@ -28,7 +85,7 @@ const Home = () => {
             <Image src={buildspaceLogo} alt="buildspace logo" />
             <p>build with buildspace</p>
           </div>
-        </a>
+        </a> */}
       </div>
     </div>
   );
